335bfd4b62c827c8301dcd5ad61f0d5503e2cf04
Gabriel Moreira <gsm.br2003@gmail.com>
2023-04-26 00:44:27 -0300

    feat: add resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,7 @@ INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Esta é uma mensagem teste para as notificações', TIMESTAMP WITH TIME ZONE '2023-07-07T03:23:11Z', false, 'aqui fica a route', 3);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Esta é uma mensagem teste para as notificações', TIMESTAMP WITH TIME ZONE '2023-07-07T03:23:11Z', false, 'aqui fica a route', 2);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Esta é uma mensagem teste para as notificações', TIMESTAMP WITH TIME ZONE '2023-07-07T03:23:11Z', false, 'aqui fica a route', 1);
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Introdução', 'Aqui vamos aprender o básico para iniciar no spring boot', 1, 'https://link-da-img-resource.com', 1, 'https://link-da-img-resource.com', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Fórum', 'Tire suas dúvidas ', 2, 'https://link-da-img-resource.com', 2, 'https://link-da-img-resource.com', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Lives', 'Assista as lives gravadas', 3, 'https://link-da-img-resource.com', 0, 'https://link-da-img-resource.com', 1);
